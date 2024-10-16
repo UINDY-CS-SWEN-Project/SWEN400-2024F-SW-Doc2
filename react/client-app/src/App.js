41b65e64231a0dd7262c1a5d552120e703b18880
bradywire <wireb@uindy.edu>
2024-10-16 11:33:52 -0400

    Updated file routes in app.js

--- react/client-app/src/App.js
+++ react/client-app/src/App.js
@@ -1,29 +1,29 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LoginHome from '../../../../../SWEN400-2024F-SW-Doc2/react/client-app/src/pages/LoginHome'
-import Home from '../../../../../SWEN400-2024F-SW-Doc2/react/client-app/src/pages/Home'
-import Registration from '../../../../../SWEN400-2024F-SW-Doc2/react/client-app/src/pages/Registration'
-import Login from '../../../../../SWEN400-2024F-SW-Doc2/react/client-app/src/pages/Login'
-import PrivateRoutes from './utils/PrivateRoutes'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginHome from './pages/LoginHome'; 
+import Home from './pages/Home'; 
+import Registration from './pages/Registration'; 
+import Login from './pages/Login'; 
+import PrivateRoutes from './utils/PrivateRoutes'; 
 import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    // GET request using fetch inside useEffect React hook
     fetch('/api/db')
-        .then((data) => {console.log(data)});
+        .then((response) => response.json()) 
+        .then((data) => { console.log(data); }); 
   }, []);
+
   return (
     <div className="App">
       <Router>
-          <Routes>
-            <Route element={<PrivateRoutes />}>
-                <Route element={<LoginHome/>} path="/home" exact/>
-                <Route element={<Registration/>} path="/registration"/>
-            </Route>
-            <Route element={<Registration/>} path="/registration"/>
-            <Route element={<Login/>} path="/login"/>
-          </Routes>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route element={<LoginHome />} path="/home" />
+          </Route>
+          <Route element={<Registration />} path="/registration" />
+          <Route element={<Login />} path="/login" />
+        </Routes>
       </Router>
     </div>
   );
