417312b1f43e80070b2211e342adb19538185171
bradywire <wireb@uindy.edu>
2024-10-23 10:36:16 -0400

    Homepage is styled better.

--- react/client-app/src/pages/Home.js
+++ react/client-app/src/pages/Home.js
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react';
+import "./Home.css";
 import { Link } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>home
-      <Link className="link" to="/teams">Teams</Link>
-    </div>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'username',
+      password: 'password',
+    };
+  }
+
+  getTeams = (event) => {
+
+  }
+
+  getDocuments = (event) => {
     
-  )
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        <header>
+          <h1>Welcome</h1>
+        </header>
+        <body>
+          <text>
+            Teams:
+            <br></br>
+          </text>
+          <div className="text_area">
+          </div>
+          <Link className="link" to="/teams">Add Team</Link>
+          <br></br>
+          <text>
+            Documents:
+            <br></br>
+          </text>
+          <div className="text_area">
+          </div>
+          <input type="submit" value="Edit Documents" className="btn" />
+        </body>
+      </div>
+    );
+  }
 }
+
+// const Home = () => {
+//   return (
+//     <div>home</div>
+//   )
+// }
 
 export default Home
--- react/client-app/src/utils/PrivateRoutes.js
+++ react/client-app/src/utils/PrivateRoutes.js
@@ -2,57 +2,9 @@ import { Outlet, Navigate } from 'react-router-dom'
 import React from 'react'
 
 
-
-// class PrivateRoutes extends React.Component {
-//     constructor(props){
-//         super(props)    
-        
-//         this.authorizeUser();
-//     }
-//         authorizeUser = () =>{
-//             const userData = {
-//                 username: this.state.username,
-//                 password: this.state.password,
-//                 name: this.state.name,
-//                 email: this.state.email
-//             };
-        
-//             fetch('http://localhost:9091/api/authorizeUser', {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                 },
-//                 body: JSON.stringify(userData),
-//             })
-//             .then(response => {
-//                 return response.json();
-//             })
-//             .then(data => {
-//                 if (data.success) {  
-//                     let auth = {'token':true}
-//                 } else {
-//                     let auth = {'token':false}
-//                     console.log('Authorization Failed:', data.message);
-//                 }
-//             })
-//             .catch((error) => {
-//                 console.error('Error:', error);
-//             });
-//         }
-        
-
-//     render(){
-//         return(
-//             auth.token ? (
-//                 <Navigate to="/home"/>
-//         )   :   (
-//                 <Navigate to="/login"/>
-//         )
-//         );
-//     }
-// }
-
 const PrivateRoutes = () => {
+    let username = localStorage.getItem('username');
+
     let auth = {'token':true} //token = true means that private route will not be blocked
     return(
         auth.token ? <Outlet/> : <Navigate to="/login"/>
